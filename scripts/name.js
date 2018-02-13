69c447f8f0ae195ddb1a72d7c6424ffa2265b66e
Joe Pea <joe@trusktr.io>
2018-02-13 12:04:26 -0800

    show the project name using a cool multi-line ascii font

---
+++ scripts/name.js
@@ -0,0 +1,26 @@
+// outputs the name of the project to stdout using a cool multi-line ascii font. See:
+// https://www.npmjs.com/package/figlet
+
+const CWD = process.cwd()
+
+const path = require('path')
+const figlet = require('figlet')
+
+const pkg = require(path.resolve(CWD, 'package.json'))
+let {name} = pkg
+
+name = name || 'yet-to-be-named-project'
+
+figlet(name, {
+
+    // TODO configurable
+    font: 'Calvin S',
+
+}, function(err, data) {
+    if (err) {
+        console.log(` --- ${name} --- `)
+        return
+    }
+
+    console.log(data)
+})
