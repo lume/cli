28b61ec9b48cf1c28d7101c486742c1e2d837451
Joe Pea <joe@trusktr.io>
2018-04-06 00:08:37 -0700

    let packages override Jest config

--- config/jest.config.js
+++ config/jest.config.js
@@ -1,12 +1,30 @@
 const CWD = process.cwd()
 const path = require('path')
+const fs = require('fs')
+const merge = require('webpack-merge')
 
-module.exports = {
+let userConfigPath = path.resolve( CWD, 'jest.config.js' )
+let userConfig = null
+
+if ( fs.existsSync( userConfigPath ) ) {
+    userConfig = require( userConfigPath )
+}
+
+let config = {
     rootDir: CWD,
+
     transform: {
         '^.+\\.js$': path.resolve(__dirname, 'babel-jest'),
     },
 
+    testPathIgnorePatterns: [
+        'node_modules',
+    ],
+
     // a great environment that supports Custom Elements
     testEnvironment: '@skatejs/ssr/jest',
 }
+
+if (userConfig) config = merge(config, userConfig)
+
+module.exports = config
