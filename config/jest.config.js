ebde686a8c197d75eaf80421684e6ffcc1c91e2f
Joe Pea <joe@trusktr.io>
2018-04-28 23:00:33 -0700

    add testMatch to jest config, so it can be extended by projects

--- config/jest.config.js
+++ config/jest.config.js
@@ -3,15 +3,13 @@ const path = require('path')
 const fs = require('fs')
 const merge = require('webpack-merge')
 
-let userConfigPath = path.resolve( CWD, 'jest.config.js' )
-let userConfig = null
-
-if ( fs.existsSync( userConfigPath ) ) {
-    userConfig = require( userConfigPath )
-}
-
 let config = {
     rootDir: CWD,
+
+    testMatch: [
+        '**/__tests__/**/*.js?(x)',
+        '**/?(*.)(spec|test).js?(x)',
+    ],
 
     transform: {
         '^.+\\.js$': path.resolve(__dirname, 'babel-jest'),
@@ -23,6 +21,13 @@ let config = {
 
     // a great environment that supports Custom Elements
     testEnvironment: '@skatejs/ssr/jest',
+}
+
+const userConfigPath = path.resolve( CWD, 'jest.config.js' )
+let userConfig = null
+
+if ( fs.existsSync( userConfigPath ) ) {
+    userConfig = require( userConfigPath )
 }
 
 if (userConfig) config = merge(config, userConfig)
