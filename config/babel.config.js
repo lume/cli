14bb191caeee9835ea59b9d2dd47eca9bedd2c7e
Joe Pea <joe@trusktr.io>
2019-04-17 20:59:51 -0700

    Simplify babel config because the modern browsers now support most of the features we're using. Nice!

--- config/babel.config.js
+++ config/babel.config.js
@@ -2,44 +2,15 @@ module.exports = {
     babelrc: false,
     plugins: [
 
-        // this has to go before transform-es2015-classes.
-        // makes super() calls to native constructors work properly. We
-        // must explicitly specify the classes we extend.
-        ['transform-builtin-classes', { globals: [ 'HTMLElement', 'Map' ] }],
-
+        // Handle re-export syntaxes not yet supported by browsers or Webpack. f.e.
+        // export * as foo from 'foo'
+        // export default from 'foo'
         '@babel/plugin-proposal-export-namespace-from',
         '@babel/plugin-proposal-export-default-from',
-        '@babel/plugin-transform-classes',
-        '@babel/plugin-transform-object-super',
+        
+        // Object rest spread will be supported in the upcoming Chromium-based
+        // Edge, then we can drop it once it has enough adoption.
         '@babel/plugin-proposal-object-rest-spread',
-        'babel-plugin-array-includes',
-
-        ['@babel/plugin-transform-runtime', {
-            helpers: true,
-            polyfill: true,
-            // we don't need regenerator, we write Promises, or
-            // transpile to Promises, and we're not currently using any
-            // generator functions.
-            regenerator: false,
-        }],
-
-        ['module:fast-async', {
-
-            //// broken in Meteor, see https://github.com/MatAtBread/nodent/issues/102
-            //compiler: {
-            //    promises: false,
-            //    noRuntime: false,
-            //},
-            //// similar to Babel transform-runtime to prevent duplication.
-            //useRuntimeModule: true,
-
-            // we'll use this for now until the above works in Meteor.  The
-            // previous config leads to better performance I think.
-            compiler: {
-                promises: true,
-                noRuntime: true,
-            },
-        }],
 
     ],
 }
