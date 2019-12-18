465447ff8d6375bf896a3f04acac9cda1b2df1b9
Joe Pea <joe@trusktr.io>
2019-12-17 23:54:53 -0800

    handle index.ts files in version-to-source

--- scripts/version-to-source.js
+++ scripts/version-to-source.js
@@ -8,10 +8,27 @@
 
 	const fs = require('fs')
 	const path = require('path')
-	const filePath = path.resolve(CWD, 'src', 'index.js')
+	let filePath = null
+	filePath = path.resolve(CWD, 'src', 'index.js')
 	const version = require(path.resolve(CWD, 'package.json')).version
 
-	let data = fs.readFileSync(filePath).toString()
+	let data = null
+
+	try {
+		data = fs.readFileSync(filePath).toString()
+	} catch (e) {
+		try {
+			filePath = path.resolve(CWD, 'src', 'index.ts')
+			data = fs.readFileSync(filePath).toString()
+		} catch (e) {}
+	}
+
+	if (!data) {
+		console.error('No index file found, or unable to read index file. Skipping version update in index file.')
+		return
+	}
+
+	process.exit()
 
 	const lines = data.trim().split('\n')
 	lines.pop() // delete last line
