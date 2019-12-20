9af715b0b00cec12816885945ae928ce7341353b
Joe Pea <joe@trusktr.io>
2019-12-20 11:59:55 -0800

    reformat version-to-source.js

--- scripts/version-to-source.js
+++ scripts/version-to-source.js
@@ -1,41 +1,36 @@
 #!/usr/bin/env node
 
-// a function wrapper is needed for older versions of Node.
-~(function() {
-	'use strict'
+const CWD = process.cwd()
 
-	const CWD = process.cwd()
+const fs = require('fs')
+const path = require('path')
+const version = require(path.resolve(CWD, 'package.json')).version
 
-	const fs = require('fs')
-	const path = require('path')
-	const version = require(path.resolve(CWD, 'package.json')).version
+let data = null
+let filePath = null
+filePath = path.resolve(CWD, 'src', 'index.js')
 
-	let data = null
-	let filePath = null
-	filePath = path.resolve(CWD, 'src', 'index.js')
-
+try {
+	data = fs.readFileSync(filePath).toString()
+} catch (e) {
 	try {
+		filePath = path.resolve(CWD, 'src', 'index.ts')
 		data = fs.readFileSync(filePath).toString()
-	} catch (e) {
-		try {
-			filePath = path.resolve(CWD, 'src', 'index.ts')
-			data = fs.readFileSync(filePath).toString()
-		} catch (e) {}
-	}
+	} catch (e) {}
+}
 
-	if (!data) {
-		console.error('No index file found, or unable to read index file. Skipping version update in index file.')
-		return
-	}
+if (!data) {
+	console.error('No index file found, or unable to read index file. Skipping version update in index file.')
+	return
+}
 
-	process.exit()
+process.exit()
 
-	const lines = data.trim().split('\n')
-	lines.pop() // delete last line
+const lines = data.trim().split('\n')
+lines.pop() // delete last line
 
-	lines.push(`export const version = '${version}'`)
+lines.push(`export const version = '${version}'`)
 
-	data = lines.join('\n') + '\n'
+data = lines.join('\n') + '\n'
 
-	fs.writeFileSync(filePath, data)
-})()
+fs.writeFileSync(filePath, data)
