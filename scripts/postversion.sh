af1bae6af9ba7e6f410676587543159fb1a51c7f
Joe Pea <joe@trusktr.io>
2019-12-20 00:50:18 -0800

    publish the dist folder of a project

--- scripts/postversion.sh
+++ scripts/postversion.sh
@@ -1,7 +1,15 @@
 set -e
 echo '--- POSTVERSION --------------------'
+echo ' -- Making production build.'
+builder run build
 echo ' -- Publish to NPM...'
+cp -f .npmignore dist/
+cp -f LICENSE dist/
+cp -f package.json dist/
+cp -f README.md dist/
+cd dist/
 npm publish
+cd ../
 echo ' -- Push to GitHub...'
 git push --follow-tags
 echo ' -- Pop any changes that were stashed...'
--- scripts/version.sh
+++ scripts/version.sh
@@ -3,6 +3,4 @@ echo '--- VERSION --------------------'
 echo ' -- Add version to source...'
 ./node_modules/builder-js-package/scripts/version-to-source.js
 git add src
-echo ' -- Making production build.'
-builder run build
 echo '--- VERSION DONE --------------------'
