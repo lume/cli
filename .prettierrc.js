9a074a9737d60e54b229f207a3c2fc0869e32b4f
Joe Pea <joe@trusktr.io>
2021-03-05 17:33:05 -0800

    set markdown formatting to tabWidth:2 to avoid strange bullet point formatting by prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,11 @@
 module.exports = {
-    tabWidth: 4,
-    useTabs: true,
-    semi: false,
-    singleQuote: true,
-    trailingComma: 'all',
-    bracketSpacing: false,
-    printWidth: 120,
-    arrowParens: 'avoid',
+	useTabs: true,
+	semi: false,
+	singleQuote: true,
+	trailingComma: 'all',
+	bracketSpacing: false,
+	printWidth: 120,
+	arrowParens: 'avoid',
+
+	overrides: [{files: '*.md', options: {tabWidth: 2}}],
 }
