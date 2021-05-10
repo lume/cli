632f7622a12be500f15a7f97ebaa585da9ae5f80
Joe Pea <joe@trusktr.io>
2021-05-09 20:40:08 -0700

    treat .d.ts files in src/ as assets to be copied to dist/

--- config/gulpfile.js
+++ config/gulpfile.js
@@ -5,5 +5,11 @@ const {src, dest} = require('gulp')
 // src/. An asset is considered to be any file other than source files. Source
 // files end in .ts or .tsx.
 exports.copyAssets = function () {
-	return src(['./src/**', '!./src/**/*.{ts,tsx}']).pipe(dest('./dist/'))
+	return src([
+		'./src/**',
+		'!./src/**/*.{ts,tsx}',
+		// Consider .d.ts files to be assets that need to be copied to dist/
+		// XXX Should we make this configurable in lume.config.js?
+		'./src/**/*.d.ts',
+	]).pipe(dest('./dist/'))
 }
