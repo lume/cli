e75572e38dd96f52721fb285954a393c0c6ba867
Joe Pea <joe@trusktr.io>
2021-06-03 11:36:51 -0700

    add comment about JSX support

--- config/babel.config.base.js
+++ config/babel.config.base.js
@@ -6,6 +6,7 @@ module.exports = {
 	].map(p => [require.resolve(p[0]), p[1]]),
 	presets: [
 		['@babel/preset-typescript', {isTSX: true, allExtensions: true, onlyRemoveTypeImports: true}],
+		// TODO update to @lume/element/babel-preset, lume/cli issue #7, lume/lume issue #219
 		['babel-preset-solid', {moduleName: '@lume/element'}],
 	].map(p => [require.resolve(p[0]), p[1]]),
 }
