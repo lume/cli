9ec3b3ab1fc7b0cb7bc163452167c31e58e7af51
Joe Pea <joe@trusktr.io>
2021-10-03 22:10:13 -0700

    improve git stash detection in release scripts

--- scripts/postversion.sh
+++ scripts/postversion.sh
@@ -1,17 +1,21 @@
 set -e
 echo '--- POSTVERSION --------------------'
+
 echo ' -- Publish to NPM...'
 # TODO a way for projects to specify --access, lume/cli #16
 npm publish --access public
+
 echo ' -- Push to GitHub...'
 git push --follow-tags
+
 echo ' -- Pop any changes that were stashed...'
-if [ -f ./node_modules/.__tmp_stash__ ]; then
+result=`git stash list | head -n 1`
+if [[ $result =~ "LUME_CLI_STASH" ]]; then
 	git stash pop || true
 	git reset HEAD
 	echo '  - Stashed changes popped.'
 else
 	echo '  - No stashed changes to pop.'
 fi
-rm -f ./node_modules/.__tmp_stash__
+
 echo '--- POSTVERSION DONE --------------------'
--- scripts/release-pre.sh
+++ scripts/release-pre.sh
@@ -1,20 +1,24 @@
 set -e
 echo '--- PREVERSION --------------------'
+
 echo ' -- Add any changes to git...'
 # TODO prompt about stashing
 git add .
+
 echo ' -- Stash any changes...'
-if [ ! -z "$(git status --porcelain)" ]; then
-	git stash
-	touch ./node_modules/.__tmp_stash__
-	echo '  - Stashed changes.'
-else
+
+result=`git stash -m LUME_CLI_STASH`
+if [[ $result =~ 'No local changes' ]]; then
 	echo '  - No changes to stash.'
-	rm -f ./node_modules/.__tmp_stash__
+else
+	echo '  - Changes stashed.'
 fi
+
 echo ' -- Clean repo...'
 # TODO don't run clean if there's no clean script.
 npm run clean
+
 echo ' -- Run tests...'
 npm test
+
 echo '--- PREVERSION DONE --------------------'
