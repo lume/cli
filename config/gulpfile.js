1f8fccc6055b9a690c2e252dbe177521c1dbb2b2
Joe Pea <joe@trusktr.io>
2023-10-14 15:41:39 -0700

    ensure dist/ is created before we copy assets to hopefully solve a problem with this in Windows CI (https://github.com/lume/lume/actions/runs/6519881972/job/17706893670)

--- config/gulpfile.js
+++ config/gulpfile.js
@@ -1,10 +1,15 @@
 // @ts-check
-const {src, dest} = require('gulp')
+const {src, dest, series} = require('gulp')
+const { promises} = require('fs')
+
+async function mkDist() {
+	await promises.mkdir('./dist')
+}
 
 // Copies assets from src/ to dist/, matching the same folder structure as in
 // src/. An asset is considered to be any file other than source files. Source
 // files end in .ts or .tsx.
-exports.copyAssets = function () {
+function copy() {
 	return src([
 		'./src/**',
 		'!./src/**/*.{ts,tsx}',
@@ -12,4 +17,7 @@ exports.copyAssets = function () {
 		// XXX Should we make this configurable in lume.config.js?
 		'./src/**/*.d.ts',
 	]).pipe(dest('./dist/'))
+
 }
+
+exports.copyAssets = series(mkDist, copy)
