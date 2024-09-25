4f0d136d1865ee6ca0d92a414a6744e35207df54
Joe Pea <joe@trusktr.io>
2024-09-25 15:10:41 -0700

    update Babel

--- config/babel.config.js
+++ config/babel.config.js
@@ -6,12 +6,14 @@ module.exports = {
 
 	plugins: [
 		['@babel/plugin-transform-typescript', {allowDeclareFields: true}],
-		['@babel/plugin-proposal-decorators', {version: '2023-05'}],
+		['@babel/plugin-proposal-decorators', {version: '2023-11'}],
 	].map(p => [require.resolve(p[0]), p[1]]),
 
 	presets: [
-		['@babel/preset-typescript', {isTSX: true, allExtensions: true, onlyRemoveTypeImports: true}],
-		// TODO update to @lume/element/babel-preset, lume/cli issue #7, lume/lume issue #219
+		[
+			'@babel/preset-typescript',
+			{isTSX: true, allExtensions: true, onlyRemoveTypeImports: true, allowDeclareFields: true},
+		],
 		['babel-preset-solid'],
 	].map(p => [require.resolve(p[0]), p[1]]),
 }
